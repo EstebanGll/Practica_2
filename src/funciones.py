bb82a6ccb282d864978f903e95734b63dab2242c
Esteeven Andres <gallegoses2000@gmail.com>
2025-04-04 21:33:04 -0300

    Actualizacion nombre archivo y actividades extra

--- src/funciones.py
+++ src/funciones.py
@@ -1,4 +1,6 @@
 import string
+import random
+import unicodedata
 
 # funcion usada para verificar si una palabra comienza con vocal
 def empieza_con_vocal(palabra):
@@ -42,6 +44,108 @@ def filtrar_lineas_por_palabra(palabra_clave, texto):
         
         if palabra_clave in palabras_limpias:
             print(linea)
+
+#4. Comprueba las condiciones dadas de un usuario ingresado
+def validacion_usuario(usuario):
+        if (len(usuario) >= 5 and
+        any(ch.isdigit() for ch in usuario) and
+        any(ch.isupper() for ch in usuario) and
+        usuario.isalnum()):
+            return "El nombre de usuario es válido."
+        else:
+            return "EL nombre de usuario no cumple con los requisitos"
+
+#5. determinar el tiempo de reaccion de un jugador si es normal, rapido o lento
+
+def categoria_tiempo_reaccion(tiempo):
+    c = "Categoria: "
+
+    if tiempo > 500:
+        return c + "Lento"
+    elif tiempo >= 200 and tiempo <= 500:
+        return c + "Normal"
+    else:
+        return c + "Rapido"
+        
+#6. encontrar menciones de alguna palabra en una lista de textos
+
+def mencion_palabra_en_lineas(palabra_ingresada, texto):
+    palabra_ingresada = palabra_ingresada.lower()
+    conteo = 0
+    for linea in texto:
+        palabras = linea.split()
+        
+        for palabra in palabras:
+            palabra = palabra.lower()
+            if palabra == palabra_ingresada:
+                conteo += 1
+    
+    return conteo
+
+#7.generar un codigo de descuento con usuario,fecha actual 
+#y caracteres alfanumericos hasta completar 30 caracteres en total
+def generador_codigo(usuario, fecha):
+    fecha_formato = fecha.strftime("%Y%m%d")
+    
+    # determino cuantos caracteres llevo al momento para ver con cuantos debo completar
+    x = int(len(usuario)+ len(fecha_formato)) 
+    
+    restante = 28 - x 
+    #28 porque dos seran los -
+    
+    #genero un string con valores random
+    caracteres=string.ascii_uppercase + string.digits
+    
+    
+    return usuario.upper() + "-" + fecha_formato.upper() + "-" +''.join(random.choices(caracteres, k= restante))
+ 
+#8. determine si dos palabras son anagramas
+def verificar_anagrama(palabra1, palabra2):
+    palabra1 = palabra1.replace(" ", "").lower()
+    palabra2 = palabra2.replace(" ", "").lower()
+    if sorted(palabra1) == sorted(palabra2):
+        return "Son anagramas."
+    else:
+        return "No son anagramas."
+
+#9. Tengo una lista de nombres y datos con errores a corregir 
+# para evitar amontonar codigo 
+# dividire cada condicion de correcion en una funcion
+
+
+def eliminar_nulos(lista):       
+    lista = [x for x in lista if x and x.strip()]
+    return lista
+
+
+    
+def eliminar_espacio_en_blanco(lista):
+    # .strip() elimina espacios al inicio y al finaly  
+    return [x.strip().replace("  ", " ") for x in lista]
+
+def palabras_formato_titulo(lista):
+        
+        lista=[x.title() for x in lista]
+        return lista
+
+def eliminar_repetidos(lista):
+
+        lista_sin_repetidos=[]
+        for x in lista:
+            if x not in lista_sin_repetidos:
+                lista_sin_repetidos.append(x)
+        return lista_sin_repetidos
+
+
+#union de todas las funciones de limpieza
+def limpieza_lista(lista):
+    
+        lista = eliminar_nulos(lista)
+        lista = eliminar_espacio_en_blanco(lista)
+        lista = palabras_formato_titulo(lista)
+        lista = eliminar_repetidos(lista)
+        return lista
+
     
                 
                 
